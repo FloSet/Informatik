f99fc21e85579b9934ea185537f2ecd2fc4b1832
FloSet <44555066+FloSet@users.noreply.github.com>
2021-03-03 15:03:19 +0100

    Remove Submodule + Update 01.03.2021

--- Java/2021_03_01/Bild/drawer.java
+++ Java/2021_03_01/Bild/drawer.java
@@ -1,32 +1,53 @@
 
 /**
- * Beschreiben Sie hier die Klasse draw.
- * 
- * @author (Ihr Name) 
- * @version (eine Versionsnummer oder ein Datum)
+ * @author Florian Schmidtke
+ * @version 03.03.2021/ 1.0.0
+
  */
 public class drawer
 {
-    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
     Triangle roof = new Triangle();
     Square window = new Square();
-    Square door = new Square();
+    Rectangle door = new Rectangle();
     Rectangle house = new Rectangle();
-    Square tree = new Square();
+    Rectangle tree = new Rectangle();
     Circle crown = new Circle();
-    
-    /**
-     * Konstruktor für Objekte der Klasse draw
-     */
+
     public drawer()
     {
-        roof.makeVisible();
         roof.changeSize(50, 130);
         roof.moveHorizontal(30);
-        roof.changeColor("red");
+        roof.changeColor("red"); 
+
+        house.changeSize(130,170);
+        house.changeColor("green");
+        house.moveHorizontal(-45);
+        house.moveVertical(15);
+
+        door.changeSize(30,60);
+        door.changeColor("black");
+        door.moveVertical(125);
+        door.moveHorizontal(40);
+
+        window.changeSize(30);
+        window.changeColor("white");
+        window.moveVertical(30);
+        window.moveHorizontal(-30);
+
+        tree.changeSize(35,90);
+        tree.moveVertical(95);
+        tree.moveHorizontal(150);
+        tree.changeColor("brown");
+
+        crown.changeSize(100);
+        crown.moveHorizontal(155);
+        crown.changeColor("green");
 
         house.makeVisible();
-        house.changeSize(130,250);
-        house.changeColor("green");
+        window.makeVisible();
+        door.makeVisible();
+        roof.makeVisible();
+        tree.makeVisible();
+        crown.makeVisible();
     }
 }
